d793b740f6597d80e728973c9dae2dfe70bc9ee4
AntiS <59616866+Anti-Sh@users.noreply.github.com>
2023-07-04 12:20:59 +0700

    Revert "Delete field STATE from create/edit device"
    This reverts commit b15574fa6d3c8b84ba62f636e0f6d6f3ad5f06f2.

--- modules/vakio/vakio_devices_edit.inc.php
+++ modules/vakio/vakio_devices_edit.inc.php
@@ -8,36 +8,36 @@
   $table_name='vakio_devices';
   $rec=SQLSelectOne("SELECT * FROM $table_name WHERE ID='$id'");
   if ($this->mode=='update') {
-    $ok=1;
-    //updating '<%LANG_TITLE%>' (varchar, required)
-    $rec['TITLE']=gr('title');
-    if ($rec['TITLE']=='') {
-      $out['ERR_TITLE']=1;
-      $ok=0;
-    }
-    //updating 'VAKIO_DEVICE_TYPE' (varchar)
-    $rec['VAKIO_DEVICE_TYPE']=gr('vakio_device_type');
-    //updating 'VAKIO_DEVICE_MQTT_TOPIC' (varchar)
-    $rec['VAKIO_DEVICE_MQTT_TOPIC']=gr('vakio_device_mqtt_topic');
-    //updating 'VAKIO_DEVICE_STATE' (varchar)
-    $rec['VAKIO_DEVICE_STATE']= "{}";
-    //updating '<%LANG_UPDATED%>' (datetime)
-    global $updated_date;
-    global $updated_minutes;
-    global $updated_hours;
-    $rec['UPDATED']=toDBDate($updated_date)." $updated_hours:$updated_minutes:00";
-    //UPDATING RECORD
-    if ($ok) {
-      if ($rec['ID']) {
-        SQLUpdate($table_name, $rec); // update
-      } else {
-        $new_rec=1;
-        $rec['ID']=SQLInsert($table_name, $rec); // adding new record
-      }
-      $out['OK']=1;
+   $ok=1;
+  //updating '<%LANG_TITLE%>' (varchar, required)
+   $rec['TITLE']=gr('title');
+   if ($rec['TITLE']=='') {
+    $out['ERR_TITLE']=1;
+    $ok=0;
+   }
+  //updating 'VAKIO_DEVICE_TYPE' (varchar)
+   $rec['VAKIO_DEVICE_TYPE']=gr('vakio_device_type');
+  //updating 'VAKIO_DEVICE_MQTT_TOPIC' (varchar)
+   $rec['VAKIO_DEVICE_MQTT_TOPIC']=gr('vakio_device_mqtt_topic');
+  //updating 'VAKIO_DEVICE_STATE' (varchar)
+   $rec['VAKIO_DEVICE_STATE']=gr('vakio_device_state');
+  //updating '<%LANG_UPDATED%>' (datetime)
+   global $updated_date;
+   global $updated_minutes;
+   global $updated_hours;
+   $rec['UPDATED']=toDBDate($updated_date)." $updated_hours:$updated_minutes:00";
+  //UPDATING RECORD
+   if ($ok) {
+    if ($rec['ID']) {
+     SQLUpdate($table_name, $rec); // update
     } else {
-      $out['ERR']=1;
+     $new_rec=1;
+     $rec['ID']=SQLInsert($table_name, $rec); // adding new record
     }
+    $out['OK']=1;
+   } else {
+    $out['ERR']=1;
+   }
   }
   if ($rec['UPDATED']!='') {
    $tmp=explode(' ', $rec['UPDATED']);
